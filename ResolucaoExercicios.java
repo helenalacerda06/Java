5eb8f9b6a76a2e475f69fda1fdc9a93696f30f99
helena.lacerda <helenacardoso062009@gmail.com>
2026-03-30 16:04:49 -0300

    exercicios

--- ResolucaoExercicios.java
+++ ResolucaoExercicios.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ResolucaoExercicios {
 
     // EXERCICIO 1
@@ -120,7 +122,7 @@ public class ResolucaoExercicios {
 
 
     // EXERCICIO 16
-    public static void stringSubistituirpalavra(){
+    public static void stringSubistituirpalavra() {
         String frase = "Meu nome é Maria";
         String palavraOriginal = "Maria";
         String palavraNova = "Helena";
@@ -129,9 +131,8 @@ public class ResolucaoExercicios {
     }
 
 
-
     // EXERCICIO 17
-    public static void stingtresPrimeiros(){
+    public static void stingtresPrimeiros() {
         String a = "1234567";
         String tresPrimeiros = a.substring(0, 3);
         System.out.println("EXERCICIO 17: " + tresPrimeiros);
@@ -139,19 +140,19 @@ public class ResolucaoExercicios {
 
 
     // EXERCICIO 18
-    public static void stingComparacao(){
+    public static void stingComparacao() {
         String a = "Eu gosto de chocolate";
         String b = "Ela gosta de frutas";
-        if (a.equals(b)){
+        if (a.equals(b)) {
             System.out.println("As frase são iguais:)");
         } else {
-            System.out.println("EXERCICIO 18: " + "As frases não são iguais:(");
+            System.out.println("EXERCICIO 18: " + "As frases não são iguais");
         }
     }
 
 
     //EXERCICIO 19
-    public static void stingRemoverEspacos(){
+    public static void stingRemoverEspacos() {
         String a = "                    :)                        ";
         String semEspaco = a.trim();
         System.out.println("EXERCICIO 19: " + semEspaco);
@@ -159,7 +160,7 @@ public class ResolucaoExercicios {
 
 
     //EXERCICIO 20
-    public static void juntarDuasStrings(){
+    public static void juntarDuasStrings() {
         String a = "Eu estudo no Acesso.";
         String b = "Lá o lanche é caro!";
         String c = a + " " + b;
@@ -168,13 +169,146 @@ public class ResolucaoExercicios {
 
 
     //EXERCICIO 21
-    public static void positivoNegativo(){
+    public static void positivoNegativo() {
         int a = -42;
-        if ( a > 0){
-            System.out.println("Número positivo:)");
-        } else if {a < 0){
-            System.out.println("Número negativo:(");
-        }
+
+        if (a > 0) {
+            System.out.println("Número positivo :)");
+        } else if (a < 0) {
+            System.out.println("EXERCICIO 21: " + "Número negativo :(");
         }
     }
+
+    //EXERCICIO 22
+    public static void parImpar() {
+        int a = 43;
+
+        if (a % 2 == 0) {
+            System.out.println("Par");
+        } else {
+            System.out.println("EXERCICIO 22: " + "impar");
+        }
+    }
+
+    //EXERCICIO 23
+    public static void aprovadoReprovado() {
+        int Helena = 70;
+        int Henrique = 50;
+
+        System.out.println("EXERCICIO 23: ");
+        if (Helena > 59) {
+            System.out.println("Nota Helena: " + Helena + " aprovada :)");
+        } else {
+            System.out.println("Nota Helena: " + Helena + " reprovada");
+        }
+
+        if (Henrique > 59) {
+            System.out.println("Nota Henrique: " + Henrique + " aprovado:)");
+        } else {
+            System.out.println("Nota Henrique: " + Henrique + " reprovado");
+        }
+    }
+
+    //EXERCICIO 24
+    public static void idadeMaiorOuMenor() {
+        int Helena = 15;
+        int Evandro = 43;
+
+        System.out.println("EXERCICIO 24: ");
+        if (Helena < 18) {
+            System.out.println("Helena é menor de idade.");
+        } else {
+            System.out.println("Helena é maior de idade.");
+        }
+
+        if (Evandro < 18) {
+            System.out.println("Evandro é menor de idade.");
+        } else {
+            System.out.println("Evandro é maior de idade.");
+        }
+    }
+
+
+    //EXERCICIO 25
+    public static void compareNumerosMostrarMaior() {
+        int a = 21;
+        int b = 48;
+
+        System.out.println("EXERCICIO 25: ");
+
+        if (a == b) {
+            System.out.print("São iguais.");
+        } else {
+            System.out.print("Não são iguais.");
+        }
+        if (a > b) {
+            System.out.print(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+
+
+    //EXERCICIO 26
+    public static void imprimirLoop(){
+        System.out.println("EXERCICIO 26: ");
+        for (int a = 1; a < 6; a++){
+            System.out.println(a);
+        }
+    }
+
+
+    //EXERCICIO 27
+    public static void imprimirLoopMaior(){
+        System.out.println("EXERCICIO 27: ");
+        for (int a =10; a > 0; a--){
+            System.out.println(a);
+        }
+    }
+
+
+    //EXERCICIO 28
+    public static void somarNumerosUmACem(){
+        System.out.println("EXERCICIO 28: ");
+
+        int soma = 0;
+        for (int a = 0; a < 101; a++){
+            soma += a;
+            System.out.print(a);
+        }
+        System.out.println(" " + soma);
+    }
+
+
+    //EXERCICIO 29
+    public static void loopPares(){
+        System.out.println("EXERCICIO 29");
+
+        for (int a = 0; a <= 20; a = a + 2){
+            System.out.println(a);
+        }
+    }
+
+
+    //EXERCICIO 30
+    public static void loopSenha(){
+        System.out.println("EXERCICIO 30: ");
+
+        Scanner a = new Scanner(System.in);
+        String correta = "1234";
+        String digitada = "";
+
+        while (!digitada.equals(correta)) {
+            System.out.println("Digite a senha.");
+            digitada = a.nextLine();
+
+            if (digitada.equals(digitada)){
+                System.out.println("Senha incorreta, tente novamente!");
+            }
+        }
+        System.out.println("Acesso liberado!");
+        a.close();
+
+    }
+
 }
